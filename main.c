c94ac7a9a3a07358fd17a38ea4180e0792a1f828
Miqueias Fernandes <miqueiasfernandes@bitbucket.com>
2020-10-28 21:53:18 -0300

    Fix memmory strtok error

--- main.c
+++ main.c
@@ -3,20 +3,30 @@
 #include <unistd.h>
 #include <limits.h>
 #include <string.h>
+#include <locale.h>
 
 char username[50], password[50];
 char cwd[PATH_MAX];
-
 void show_login()
 {
+    typedef struct
+    {
+        char *user;
+        char *password;
+    } * user;
+    char *user_data_column;
+    user *user_data;
+
     getcwd(cwd, sizeof(cwd));
     strcat(cwd, "/data/Users.csv");
+
     FILE *users_list = fopen(cwd, "r");
     long file_length = ftell(users_list);
     char *buffer = malloc(file_length);
-    char line[PATH_MAX]; 
+    char line[PATH_MAX];
     int line_counter = 0;
     int is_authorized = 0;
+
     printf("\bLogin\n");
     printf("Nome do usuário: ");
     fgets(username, sizeof(username), stdin);
@@ -24,16 +34,20 @@ void show_login()
     fgets(password, sizeof(password), stdin);
     if (users_list)
     {
-        // if (buffer)
-        // {
-        //     while (fgets(line, sizeof(line), users_list) != NULL)
-        //     {
-        //         if(){
-                    
-        //         }
-        //         line_counter++;
-        //     }
-        // }
+        if (buffer)
+        {
+            while (fgets(line, sizeof(line), users_list) != NULL)
+            {
+                if (line_counter == 0)
+                {
+                    line_counter++;
+
+                    continue;
+                }
+                user_data_column = strtok(line, ",");
+                printf("Ok");
+            }
+        }
     }
     else
     {
