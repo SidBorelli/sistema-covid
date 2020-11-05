b12c5adaff681bb073e6d3ce3ba4afa4ef769d26
Miqueias Fernandes <miqueiasfernandes@bitbucket.com>
2020-11-04 22:26:54 -0300

    Insert menu and record list at main screen

--- main.c
+++ main.c
@@ -5,7 +5,15 @@
 #include <string.h>
 #include <locale.h>
 
-char username[50], password[50];
+#define ANSI_COLOR_RED "\x1b[31m"
+#define ANSI_COLOR_GREEN "\x1b[32m"
+#define ANSI_COLOR_YELLOW "\x1b[33m"
+#define ANSI_COLOR_BLUE "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN "\x1b[36m"
+#define ANSI_COLOR_RESET "\x1b[0m"
+#define INPUT_MAX_LENGTH 50
+char username[INPUT_MAX_LENGTH], password[INPUT_MAX_LENGTH];
 int username_match = 0;
 int password_match = 0;
 int is_authorized = 0;
@@ -29,7 +37,7 @@ void show_login()
     char line[PATH_MAX];
     int line_counter = 0;
 
-    printf("\bLogin\n");
+    printf(ANSI_COLOR_RED "Login" ANSI_COLOR_RESET "\n");
     printf("Nome do usuário: ");
     fgets(username, sizeof(username), stdin);
     printf("Senha: ");
@@ -96,12 +104,12 @@ void show_login()
     if (username_match == 1 && password_match == 1)
     {
         printf("Usuário autenticado\n");
+        is_authorized = 1;
 #ifdef WIN32
         system("pause");
 #else
         system("read -p \"Pressione enter para sair...\" saindo");
 #endif
-        exit(1);
     }
     else
     {
@@ -115,7 +123,16 @@ void show_login()
     }
 }
 
-void show_menu()
+void clear()
+{
+#ifdef WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
+}
+
+void verify_if_is_authorized()
 {
     if (is_authorized == 0)
     {
@@ -131,7 +148,58 @@ void show_menu()
 
 void show_menu()
 {
+    char *ocurrences_data_column;
+    clear();
     verify_if_is_authorized();
+    printf(ANSI_COLOR_BLUE "Ferramenta de Monitoramento de Casos de COVID-19" ANSI_COLOR_RESET "\n");
+    for (int vertical_line_counter = 0; vertical_line_counter < 30; vertical_line_counter++)
+    {
+        printf(ANSI_COLOR_BLUE "\u2500");
+    }
+    printf(ANSI_COLOR_RESET, "\n");
+    getcwd(cwd, sizeof(cwd));
+    strcat(cwd, "/data/Ocurrences.csv");
+    FILE *ocurrences_list = fopen(cwd, "r");
+    char line[PATH_MAX];
+    if (sizeof(ocurrences_list) <= 1)
+    {
+        printf(ANSI_COLOR_RED "\nNenhum registro cadastrado" ANSI_COLOR_RESET "\n\n");
+    }
+    else
+    {
+        printf(ANSI_COLOR_CYAN "\nLista de Registros: " ANSI_COLOR_RESET "\n\n");
+        int line_counter = 0;
+        while (fgets(line, sizeof(line), ocurrences_list) != NULL)
+        {
+            if (line_counter == 0)
+            {
+                line_counter++;
+                continue;
+            }
+            int ocurrences_column_counter = 1;
+            printf(ANSI_COLOR_CYAN "Id: 1 - " ANSI_COLOR_RESET);
+            ocurrences_data_column = strtok(line, ",");
+            while (ocurrences_data_column != NULL && ocurrences_column_counter <= 5)
+            {
+                printf("%s | ", ocurrences_data_column);
+                ocurrences_data_column = strtok(NULL, ",\n");
+            }
+            line_counter++;
+        }
+    }
+    int selected_option;
+    for (int vertical_line_counter = 0; vertical_line_counter < 30; vertical_line_counter++)
+    {
+        printf(ANSI_COLOR_BLUE "\u2500");
+    }
+    printf(ANSI_COLOR_RESET, "\n");
+    printf(ANSI_COLOR_YELLOW "\nOpçoes:" ANSI_COLOR_RESET "\n\n");
+    printf(ANSI_COLOR_YELLOW "  1 - Ver todos os registros" ANSI_COLOR_RESET "\n");
+    printf(ANSI_COLOR_YELLOW "  2 - Ver casos de risco" ANSI_COLOR_RESET "\n");
+    printf(ANSI_COLOR_YELLOW "  3 - Inserir novo caso" ANSI_COLOR_RESET "\n");
+    printf(ANSI_COLOR_YELLOW "  0 - Sair" ANSI_COLOR_RESET "\n");
+    printf("\n\nSelecione o número de uma opção do menu: ");
+    fgets(&selected_option, sizeof(selected_option), stdin);
 }
 
 int main()
